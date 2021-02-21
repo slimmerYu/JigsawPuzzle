f58457aba202fa261adfe4464bb530d7828e6344
slimmerYu <925320747@qq.com>
2021-02-21 15:55:10 +0800

    添加路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@
  * @Author: slimmerYu
  * @Date: 2021-01-06 19:53:02
  * @LastEditors: slimmerYu
- * @LastEditTime: 2021-01-28 15:44:45
+ * @LastEditTime: 2021-02-19 23:02:23
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
@@ -12,6 +12,7 @@ import Home from 'views/home/Home.vue'
 import Login from 'views/Login.vue'
 
 Vue.use(VueRouter)
+
 
 const routes = [
   {
@@ -32,5 +33,16 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 从哪个路径跳转而来
+  // next 是一个函数, 表示放行
+  //    next()  放行, next('/login) 强制跳转
+  if (to.path === '/login')  return next()
+  // 获取token
+  const tokenStr = sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
