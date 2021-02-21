19bcd6b93cc8dba487312414378fd42c434a74cc
slimmerYu <925320747@qq.com>
2021-02-21 15:49:35 +0800

    解决刷新后state清空问题

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
  * @Author: slimmerYu
  * @Date: 2021-01-06 19:53:02
  * @LastEditors: slimmerYu
- * @LastEditTime: 2021-01-07 23:26:27
+ * @LastEditTime: 2021-02-21 11:43:38
 -->
 <template>
   <v-app>
     <v-main>
-      <Navbar v-show="$route.path!=='/login'"/>
+      <Navbar v-show="$route.path !== '/login'" />
       <router-view></router-view>
     </v-main>
   </v-app>
@@ -17,18 +17,34 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld';
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    Navbar
+    Navbar,
   },
-  data: () => ({ 
-    showNav: false//控制Navbar的显示,在登录页不显示,用户成功登录后显示
+  data: () => ({
+    showNav: false, //控制Navbar的显示,在登录页不显示,用户成功登录后显示
   }),
-  watch: {
-   
-  }
+  created() {
+    
+    //在页面加载时读取sessionStorage里的状态信息
+
+    if (sessionStorage.getItem("store")) {
+      this.$store.replaceState(
+        Object.assign(
+          {},
+          this.$store.state,
+          JSON.parse(sessionStorage.getItem("store"))
+        )
+      );
+    }
+
+    //在页面刷新时将vuex里的信息保存到sessionStorage里
+    window.addEventListener("beforeunload", () => {
+      sessionStorage.setItem("store", JSON.stringify(this.$store.state));
+    });
+  },
 };
 </script>
